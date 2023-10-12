d59adbf8b30b9dc90ede6f0376f8d2965846c275
christine <christine.clark@uleth.ca>
2023-10-12 17:18:22 -0600

    added APOD API

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,38 @@
-console.log("Hello World!")
+const xhr = new XMLHttpRequest();
 
-document.getElementById("prev").style.background = "#4a4a4a"
+const baseURL = "https://api.nasa.gov/planetary/apod"
+const query = "?api_key=APIKEYHERE&count=5"
+const url = baseURL + query;
+var APOD_array;
+
+xhr.open("GET", url, true);
+xhr.send(null);
+
+xhr.onload = function () {
+    console.log("it worked!")
+    APOD_array = JSON.parse(xhr.responseText);
+    console.log(APOD_array);
+    createAPOD();
+}
 
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-//const APODs = document.getElementsByClassName("APOD");
-//console.log(APODs)
+
 let index = 0;
-
-// const APODs = ["imgs/Hourglass_HubblePathak_1080.jpg", "imgs/MoValleyEclipse1024.jpg", "imgs/WitchHead_Alharbi_1080.jpg"]
-// console.log(APODs)
-// document.getElementById("APOD__group").innerHTML = `<img class="APOD" src="${APODs[0]}">`
-
-function APOD_obj(source, date, img, title) {
-    this.source = source,
-        this.date = date,
-        this.img = img,
-        this.title = title
-}
-const APOD_oct3 = new APOD_obj("Image Credit & Copyright: NASA, ESA, Hubble, HLA; Processing & Copyright: Harshwardhan Pathak", "03-10-2023", "imgs/Hourglass_HubblePathak_1080.jpg", "MyCn 18: The Engraved Hourglass Planetary Nebula")
-const APOD_oct4 = new APOD_obj("Image Credit & Copyright: Abdullah Alharbi", "04-10-2023", "imgs/WitchHead_Alharbi_1080.jpg", "IC 2118: The Witch Head Nebula")
-const APOD_oct5 = new APOD_obj("Image Credit & Copyright: Tunc Tezel (TWAN)", "05-10-2023", "imgs/MoValleyEclipse1024.jpg", "Ring of Fire over Monument Valley")
-
-const APOD_array = [APOD_oct3, APOD_oct4, APOD_oct5]
-console.log(APOD_array)
 
 function createAPOD() {
     const APOD = document.createElement("div");
     APOD.className = "APOD";
     APOD.innerHTML = "<figure>"
-        + "<img src='" + APOD_array[index].img + "'>"
-        + "<p>" + APOD_array[index].source + "</p>"
+        + "<img src='" + APOD_array[index].url + "'>"
+        + "<p>" + APOD_array[index].copyright + "</p>"
         + "</figure>"
         + "<div><p>" + APOD_array[index].date + "</p>"
         + "<h2>" + APOD_array[index].title + "</h2>";
     document.getElementById("APOD__group").innerHTML = "";
     document.getElementById("APOD__group").appendChild(APOD);
 }
-createAPOD();
+// createAPOD();
 
 prev.innerHTML = "&larr; go back";
 
